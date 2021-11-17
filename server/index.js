3feb122670c8f477a3f2aed98e5153c7b38b2767
Icarus Romero <cloudkingdom07@yahoo.com>
2021-11-17 15:22:52 -0600

    made client folder and moved files there

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 
